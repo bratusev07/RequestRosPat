2a4775902953151ef68a9d2879a2deeb60491f2d
bratusevd <bratusevd@mail.ru>
2022-09-21 17:52:44 +0300

    Добавил конструктор для поиска похожих по куску текста

--- app/src/main/java/com/example/requestrospat/models/SameModel.java
+++ app/src/main/java/com/example/requestrospat/models/SameModel.java
@@ -3,11 +3,18 @@ package com.example.requestrospat.models;
 public class SameModel {
     private String type_search;
     private String pat_id;
+    private String pat_text;
     private int count;
 
     public SameModel(String pat_id, int count) {
         this.type_search = "id_search";
         this.pat_id = pat_id;
+        this.count = count;
+    }
+
+    public SameModel(String pat_text, int count, char x) {
+        this.type_search = "text_search";
+        this.pat_text = pat_text;
         this.count = count;
     }
 
